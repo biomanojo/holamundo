fdbbb13a7da2ead58ac1b22b5991d031c6ff974f
juan carlos <juancarlos.gomez017@gmail.com>
2016-08-08 20:41:51 -0500

    clase2

---
+++ holamundo.py
@@ -0,0 +1,98 @@
+
+# coding: utf-8
+
+# 
+# print"hola mundo"
+# 
+# $$\int_x^\infty\frac{dt}{t(t^2-1)log(t)}$$
+
+# In[3]:
+
+2+4*6/8.0-3
+
+
+# In[4]:
+
+3/2
+
+
+# In[6]:
+
+(3+5j)-(1-1j)
+
+
+# In[9]:
+
+abs(3+5j)
+
+
+# In[8]:
+
+5**3
+
+
+# In[10]:
+
+int(_9)
+
+
+# In[11]:
+
+float(_9)
+
+
+# In[13]:
+
+complex(_9)
+
+
+# In[14]:
+
+round(_9)
+
+
+# In[15]:
+
+max(10,20,30)
+
+
+# In[16]:
+
+num = 10
+
+
+# In[17]:
+
+num 
+
+
+# In[30]:
+
+x,y,z =a,b,c
+    print(x,y,z)
+
+
+# In[22]:
+
+x
+
+
+# In[24]:
+
+x
+
+
+# In[29]:
+
+x=5
+y=0
+if x > y:
+    print("el mayor es ", x)
+else:
+    print("el menor es ", y)
+
+
+# In[ ]:
+
+
+
